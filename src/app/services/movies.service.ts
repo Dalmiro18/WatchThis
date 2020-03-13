430697774b600c1af011c97db50ad645cba0fa71
dalmiroflores <dalmiro.flores@smartraining.cl>
2020-03-13 12:38:59 -0300

    repo

---
+++ src/app/services/movies.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import{HttpClient} from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MoviesService {
+
+  constructor(private http: HttpClient) { }
+
+  getFeature(){
+    return this.http.get(`https://api.themoviedb.org/3/discover/movie?primary_release_date.gte=2014-09-15&primary_release_date.lte=2014-10-22&api_key=04d3a9add65f0f3926b167628b1fd575&language=es&include_image_language=es`)
+  }
+}
